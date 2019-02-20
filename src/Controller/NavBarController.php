04e0c65f5430e5201d98cc653d01fff55a43df0c
Devin Gunay <devingunay@gmail.com>
2019-02-19 22:18:03 -0800

    Add frontpage spacer, add navbar links

--- src/Controller/NavBarController.php
+++ src/Controller/NavBarController.php
@@ -5,22 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Controls NavBar rendering by passing info about the current active page and
+ * which links go in the NavBar.
+ */
 class NavBarController extends AbstractController
 {
   protected const NAVBAR_LINKS = [
-    '/goodbans', 
-    '/hobbies',  
-    '/about',    
-    '/blog',     
+    'Good Bans' => '/goodbans', 
+    'Hobbies'   => '/hobbies',  
+    'About'     => '/about',    
+    'Blog'      => '/blog',     
   ];
 
-  public function renderWithCurrentPageLinkDeactivated(Request $request)
+  /**
+   * @param Request $originalRequest Passed via twig {{app.request}}
+   * @return void
+   */
+  public function renderWithCurrentPageLinkDeactivated(Request $originalRequest)
   { 
-    // TODO: pull the current page out from the request so that the template
-    // can deactivate it in the navbar
-    $currentPageUri = '';
     return $this->render('navbar.html.twig', [
-      'currentPage' => $currentPageUri,
+      'currentPage' => $originalRequest->getPathInfo(),
       'navBarLinks' => self::NAVBAR_LINKS,
     ]);
   }
