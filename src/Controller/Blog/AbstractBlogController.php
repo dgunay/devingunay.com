5ddbe61ed25147019434cc10e96139565390c72c
Devin Gunay <devingunay@gmail.com>
2019-02-24 23:34:24 -0800

    Rename BlogController to AbstractBlogController

---
+++ src/Controller/Blog/AbstractBlogController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller\Blog;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use BlogBackend\Archive;
+
+use Parsedown;
+
+/**
+ * Handles data and logic common to all Blog controller classes.
+ */
+abstract class AbstractBlogController extends AbstractController
+{
+  // TODO: the .env file doesn't seem to be behaving as I expected so I 
+  // hardcoded these here. They really shouldn't be.
+  protected const PUBLISHED_POSTS_DIR = __DIR__ . '/../../../blog/published';
+  protected const FLAT_ARCHIVE_FILE   = __DIR__ . '/../../../blog/flat_archive.json';
+  protected const YMD_ARCHIVE_FILE    = __DIR__ . '/../../../blog/ymd_archive.json';
+
+  /** @var Archive $archive */
+  protected $archive;
+
+  /** @var Parsedown $parsedown */
+  protected $parsedown;
+
+  public function __construct()
+  {
+    $this->archive = new Archive(
+      self::PUBLISHED_POSTS_DIR,
+      self::FLAT_ARCHIVE_FILE,
+      self::YMD_ARCHIVE_FILE
+    );
+
+    $this->parsedown = new Parsedown();
+  }
+
+  /**
+   * Returns the most recent posts in the archive.
+   *
+   * @param integer $n
+   * @return Post[]
+   */
+  protected function mostRecentPosts(int $n = 5) : array {
+    $this->archive->loadFlatArchive();
+    $most_recent_posts = array_slice(
+      $this->archive->getFlatArchive(),
+      0,
+      $n,
+      true // preserve keys for timestamps
+    );
+
+    return $most_recent_posts;
+  }
+}
+
